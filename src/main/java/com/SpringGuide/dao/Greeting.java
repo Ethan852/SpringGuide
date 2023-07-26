47534a7e05996f783d9733fb52eb868a8efa0f00
ethan_kp_wan <ethan_kp_wan@manulife.com>
2023-07-26 14:34:07 +0800

    before revert

---
+++ src/main/java/com/SpringGuide/dao/Greeting.java
@@ -0,0 +1,12 @@
+package com.SpringGuide.dao;
+
+public class Greeting {
+    long id;
+    String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+}
